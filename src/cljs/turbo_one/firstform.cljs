56a6b1173fa300c00b4318cf3a167a1fa14ceaf9
Timothy Roy <roy.tim@gmail.com>
2015-03-31 19:12:22 -0500

    further progress

--- src/cljs/turbo_one/firstform.cljs
+++ src/cljs/turbo_one/firstform.cljs
@@ -1,8 +1,7 @@
 (ns turbo-one.firstform)
 
 
-(defn firstform []
-  [:div
+(defn header []
    [:h4.center-children "NO. _________________"]
    [:table.center
     [:tr
@@ -32,15 +31,35 @@
     [:tr
      [:td "DEFENDANT."]
      [:td ")"]
-     [:td ""]]]
+     [:td ""]]])
 
+(defn firstform []
+  [:div
+   [header]
    [:p.center-children [:strong [:ins "PLAINTIFF'S ORIGINAL PETITION"]]]
    [:p "TO THE HONORABLE JUDGE OF THE COURT:"]
    [:p "     Plaintiff files this original petition in the the above-styled and numbered cause, and in support, shows the Court as follows:"]
+   
    [:p.center-children [:b "I. DISCOVERY"]] 
-   [:ol 
-    [:li "Plaintiff intends to conduct discovery under Level 1."]
+   [:ol
+    [:li "Plaintiff intends to conduct discovery under Level 1."]]
+    [:p.center-children [:b "II. PARTIES"]]
+   [:ol {:start 2}
     [:li [:input {:type "text" :value "Your name here"}] ", Plaintiff, is a resident of the county of this Court, within Texas."]
-    [:li [:input {:type "text" :value "Your landlord's name here"}] ", Defendant, may be served at the following address:" [:input {:type "text" :value "Your landlord's address here"}]]
+    [:li [:input {:type "text" :value "Your landlord's name here"}] ", Defendant, may be served at the following address:" [:input {:type "text" :value "Your landlord's address here"}]]]
+   [:p.center-children [:b "III. JURISDICTION AND VENUE"]]
+   [:ol {:start 4}
     [:li "The amount in controversy is within the jurisdictional limits of of this Court, and venue is proper as the cause of action arose in the county of this Court within Texas."]]
+   [:p.center-children [:b "IV. FACTS AND CAUSES OF ACTION"]]
+   [:ol {:start 5}
+    [:li "Plaintiff is a tenant and leases premises from Defendant."]
+    [:li "The following events occurred in violation of the law: " [:textarea.notes {:rows 4 :cols 70 :value "Just the facts, ma'am"}]]]
+   [:p.center-children [:b "V. REQUEST FOR RELIEF"]]
+   [:p "Plaintiff requests this Court grant the following relief: "]
+   [:ol {:type "a"}
+    [:li "actual damages;"]
+    [:li "civil penalties if available under law;"]
+    [:li "court costs; and"]
+    [:li "any other relief to which Plaintiff is entitled."]]
+   
    ])
